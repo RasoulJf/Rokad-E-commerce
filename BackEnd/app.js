7f45c754c7a5d51044942fb019f4085af076ebc9
RasoulJf <rasouljalilian8@gmail.com>
2025-02-17 10:07:28 +0330

    Create BackEnd Base File

---
+++ BackEnd/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import catchError from "./Utils/catchError.js";
+import HandleERROR from "./Utils/handleError.js";
+import path from "path";
+import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("Public"));
+
+app.use("*", (req, res, next) => {
+  next(new HandleERROR("Route not Found", 404));
+});
+app.use(catchError);
+
+export default app;
