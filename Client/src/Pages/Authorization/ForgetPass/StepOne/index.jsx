72a64fbd8c6c96948d1a0dc2749ab949402f8f02
RasoulJf <rasouljalilian8@gmail.com>
2025-02-22 11:29:25 +0330

    Update index.jsx

--- Client/src/Pages/Authorization/ForgetPass/StepOne/index.jsx
+++ Client/src/Pages/Authorization/ForgetPass/StepOne/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import fetchData from "../../../../Utils/fetchData";
+import notify from "../../../../Utils/notify";
 
 export default function ForgetPassStepOne() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [phoneNumber, setPhoneNumber] = useState();
+  const handleSubmit = async () => {
+    if (!phoneNumber) {
+      return notify("please enter your phone number", "error");
+    }
+    const res = await fetchData("auth/resend", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ phoneNumber }),
+    });
+    if (res.success) {
+      notify(res.message, "success");
+    } else {
+      notify(res.message, "error");
+    }
+  };
+  return <div></div>;
 }
