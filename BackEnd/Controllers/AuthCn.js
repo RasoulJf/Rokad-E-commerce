37f04f2cde26101a0d1fa18ff4f48b18659d6c9b
RasoulJf <rasouljalilian8@gmail.com>
2025-02-22 10:57:29 +0330

    Update AuthCn.js

--- BackEnd/Controllers/AuthCn.js
+++ BackEnd/Controllers/AuthCn.js
@@ -1,47 +1,134 @@
-import User from "../Models/UserMd";
+import User from "../Models/UserMd.js";
 import catchAsync from "../Utils/catchAsync.js";
 import HandleERROR from "../Utils/handleError.js";
-import { sendAuthCode } from "../Utils/smsHandler.js";
-
-export const auth = catchAsync(async (req,res,next) => {
-    const {phoneNumber=null} = req.body
-    if(!phoneNumber){
-        return next(new HandleERROR('Phone Number is Require',400))
+import { sendAuthCode, verifyCode } from "../Utils/smsHandler.js";
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+export const auth = catchAsync(async (req, res, next) => {
+    const { phonNumber = null } = req.body
+    if (!phonNumber) {
+        return next(new HandleERROR("PhoneNumber is required", 400))
     }
-    const user = await User.findOne({phoneNumber})
-    if(user&&user?.password){
-return res.status(200).json({
-    success:true,
-    newAccount:false,
-    password:true
-})
-    }else{
-        const resultSms = await sendAuthCode(phoneNumber)
-        if(resultSms?.success){
-            return res.status(200).json({
-                success:true,
-                newAccount:user?._id?false:true,
-                password:false,
-                message:'Code Sent' 
-            })
-        }else{
-            return res.status(200).json({
-                success:true,
-                newAccount:user?._id?false:true,
-                password:false,
-                message:'Check Phone Number and try again' 
+    const user = await User.findOne({ phonNumber })
+    if (user && user?.password) {
+        return res.status(200).json({
+            success: true,
+            newAccount: false,
+            password: true
         })
-    }}
+    } else {
+        const resultSms = await sendAuthCode(phonNumber)
+        if (resultSms?.success) {
+            return res.status(200).json({
+                success: true,
+                newAccount: user?._id ? false : true,
+                password: false,
+                message: "code sent"
+            })
+        } else {
+            return res.status(200).json({
+                success: true,
+                newAccount: user._id ? false : true,
+                password: false,
+                message: "check phone number and try again"
+            })
+        }
+    }
 })
-export const checkOtp = catchAsync(async (req,res,next) => {
-   
+export const checkOtp = catchAsync(async (req, res, next) => {
+    const { phoneNumber = null, code = null, newAccount = 'unknown' } = req.body
+    if (!phoneNumber || !code || newAccount == "unknown") {
+        return next(new HandleERROR('phoneNumber and newAccount and password is required', 400))
+    }
+    const verifyResult = await verifyCode(phoneNumber, code)
+    if (!verifyResult.success) {
+        return next(new HandleERROR("invalid code", 400))
+    }
+    let user;
+    if (newAccount) {
+        user = await User.create({ phoneNumber })
+    } else {
+        user = await User.findOne({ phoneNumber })
+    }
+    const token = jwt.sign({ id: user._id, role: user.role, phoneNumber: user.phoneNumber }, process.env.JWT_SECRET)
+    return res.status(newAccount ? 201 : 200).json({
+        success: true,
+        message: newAccount ? 'register successfully' : 'login successfully',
+        token,
+        data: {
+            phoneNumber,
+            id: user._id,
+            role: user.role,
+            favoriteProduct: user?.favoriteProduct,
+            fullName: user?.fullName
+        }
+    })
 })
-export const checkPassword = catchAsync(async (req,res,next) => {
-   
+export const checkPassword = catchAsync(async (req, res, next) => {
+    const { phoneNumber = null, password = null } = req.body
+    if (!phoneNumber || !code) {
+        return next(new HandleERROR('phoneNumber and password is required', 400))
+    }
+    const user = await User.findOne({ phoneNumber })
+    if (!user) {
+        return next(new HandleERROR('user not exist', 400))
+    }
+    const validPassword = bcryptjs.compareSync(password, user.password)
+    if (!validPassword) {
+        return next(new HandleERROR('password is incorrect', 400))
+    }
+    const token = jwt.sign({ id: user._id, role: user.role, phoneNumber: user.phoneNumber }, process.env.JWT_SECRET)
+    return res.status(200).json({
+        success: true,
+        message: "login successfully",
+        token,
+        data: {
+            phoneNumber,
+            id: user._id,
+            role: user.role,
+            favoriteProduct: user?.favoriteProduct,
+            fullName: user?.fullName
+        }
+    })
 })
-export const forgetPassword = catchAsync(async (req,res,next) => {
-   
+export const forgetPassword = catchAsync(async (req, res, next) => {
+    const { phoneNumber = null, code = null, password = null } = req.body
+    if (!phoneNumber || !code) {
+        return next(new HandleERROR('phoneNumber ande code and password is required', 400))
+    }
+    const verifyResult = await verifyCode(phoneNumber, code)
+    if (!verifyResult.success) {
+        return next(new HandleERROR("invalid code", 400))
+    }
+    const regex = new RegExp = (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,$/)
+    if (!regex.test(password)) {
+        return next(new HandleERROR('invalid password', 400))
+    }
+    const hashPassword = bcryptjs.hashSync(password, 10)
+    const user = await User.findOneAndUpdate({ phoneNumber }, { password: hashPassword })
+    const token = jwt.sign({ id: user._id, role: user.role, phoneNumber: user.phoneNumber }, process.env.JWT_SECRET)
+    return res.status(200).json({
+        success: true,
+        message: "login successfully",
+        token,
+        data: {
+            phoneNumber,
+            id: user._id,
+            role: user.role,
+            favoriteProduct: user?.favoriteProduct,
+            fullName: user?.fullName
+        }
+    })
 })
-export const resendCode = catchAsync(async (req,res,next) => {
-   
+export const resendCode = catchAsync(async (req, res, next) => {
+    const { phonNumber = null } = req.body
+    if (!phonNumber) {
+        return next(new HandleERROR("PhoneNumber is required", 400))
+    }
+    const resultSms = await sendAuthCode(phonNumber)
+    return res.status(newAccount ? 201 : 200).json({
+        success: resultSms.success,
+        message: resultSms.success ? 'code sent' : resultSms.message,
+
+    })
 })
