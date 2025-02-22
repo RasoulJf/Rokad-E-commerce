ca2f9b7eb08e08ac78879d0674c745a15678473c
RasoulJf <rasouljalilian8@gmail.com>
2025-02-22 11:57:40 +0330

    Update index.jsx

--- Client/src/Pages/Authorization/index.jsx
+++ Client/src/Pages/Authorization/index.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 export default function Authorization() {
   return (
-    <div className='flex justify-center items-center h-[100vh]'>
+    <>
       <Outlet/>
-    </div>
+    </>
   )
 }
