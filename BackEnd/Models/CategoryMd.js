9cb70d5063a2c12a21d3bca5bfee4ee3d4bb67b7
RasoulJf <rasouljalilian8@gmail.com>
2025-02-22 10:57:32 +0330

    Update CategoryMd.js

--- BackEnd/Models/CategoryMd.js
+++ BackEnd/Models/CategoryMd.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
-const categorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required:[true,'Title is Required']
-  },
-  image: {
-    type: String
-  },
-  subCategory: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Category'
-  }
-},{timestamps:true});
-const Category=mongoose.model("Category",categorySchema)
+const categorySchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,"title is required"]
+    },
+    image:{
+        type:String
+    },
+    subCategory:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Category"
+    }
+})
+const Category=mongoose.Schema("Category",categorySchema)
 export default Category
