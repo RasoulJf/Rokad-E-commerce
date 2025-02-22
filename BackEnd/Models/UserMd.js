2f821cf458b526d7e6b85129364027f1986e1a5e
RasoulJf <rasouljalilian8@gmail.com>
2025-02-22 10:57:39 +0330

    Update UserMd.js

--- BackEnd/Models/UserMd.js
+++ BackEnd/Models/UserMd.js
@@ -1,29 +1,29 @@
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String
-  },
-  username: {
-    type: String
-  },
-  password: {
-    type: String
-  },
-  phoneNumber:{
-    type:String,
-    required :[true,'Phone Number Is Required'],
-    match: [/^(\+98|0)?9\d{9}$/,'Phone Number Ivalid']
-  },
-  favProducts:{
-type: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'Product'
-}]
-  },
-  isComplete:{
-    type:Boolean,
-    default:false
-  }
-},{timestamps:true});
-const User=mongoose.model("User",userSchema)
+const userSchema=new mongoose.Schema({
+    fullname:{
+        type:String
+    },
+    username:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    phoneNumber:{
+        type:String,
+        required :[true,'Phone Number Is Required'],
+        match: [/^(\+98|0)?9\d{9}$/,'Phone Number Ivalid']
+    },
+    favoriteProducts:{
+        type:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Product"
+        }]
+    },
+    isComplete:{
+        type:Boolean,
+        default:false
+    }
+})
+const User=mongoose.Schema("User",userSchema)
 export default User
