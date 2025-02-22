403779df3795ff2fa5e1e12ccd898b7d59adbbe5
RasoulJf <rasouljalilian8@gmail.com>
2025-02-22 11:22:54 +0330

    Update index.jsx

--- Client/src/Pages/Authorization/index.jsx
+++ Client/src/Pages/Authorization/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
-export default function ForgetPass() {
+export default function Authorization() {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center h-[100vh]'>
+      <Outlet/>
     </div>
   )
 }
