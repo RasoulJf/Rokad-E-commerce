b4695867b5de73057f0313c0518000c044ced76b
RasoulJf <rasouljalilian8@gmail.com>
2025-03-01 12:43:12 +0330

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,6 +5,7 @@ import HandleERROR from "./Utils/handleError.js";
 import path from "path";
 import cors from "cors";
 import authRouter from "./Routes/auth.js";
+import userRouter from "./Routes/User.js";
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("Public"));
 app.use('/api/auth',authRouter)
+app.use('/api/users',userRouter)
 app.use("*", (req, res, next) => {
   next(new HandleERROR("Route not Found", 404));
 });
