6f6cc4f308026d58e9794d92fb23b294ea484552
RasoulJf <rasouljalilian8@gmail.com>
2025-03-01 11:25:24 +0330

    Update index.jsx

--- Client/src/Pages/Authorization/ForgetPass/index.jsx
+++ Client/src/Pages/Authorization/ForgetPass/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ForgetPassStepOne from './StepOne'
+import ForgetPassStepTwo from './StepTwo'
 
 export default function ForgetPass() {
+  const [step,setStep]= useState(1)
+  const handleStep = (st) => {
+    setStep(st)
+  }
+  
   return (
-    <div>
-      
-    </div>
+    <>
+      {step ==1 ? <ForgetPassStepOne handleStep = {handleStep}/> : <ForgetPassStepTwo handleStep = {handleStep}/>}
+    </>
   )
 }
