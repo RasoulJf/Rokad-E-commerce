57b526c714f4d366cc96db3d6b3595dcc299ab33
RasoulJf <rasouljalilian8@gmail.com>
2025-04-05 11:54:34 +0330

    Update ProductCn.js

---
+++ BackEnd/Controllers/ProductCn.js
@@ -0,0 +1,87 @@
+import Product from "../Models/ProductMd.js";
+import ProductVariant from "../Models/ProductVariantMd.js";
+import ApiFeatures from "../Utils/apiFeatures.js";
+import catchAsync from "../Utils/catchAsync.js";
+import HandleERROR from "../Utils/handleError.js";
+import fs from "fs";
+import { __dirname } from "../app.js";
+export const createProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.create(req.body);
+  return res.status(201).json({
+    success: true,
+    message: "Product Created Successfully",
+    data: product,
+  });
+});
+
+export const getAllProducts = catchAsync(async (req, res, next) => {
+  const features = new ApiFeatures(Product, req.query)
+    .filter()
+    .limitFields()
+    .sort()
+    .paginate()
+    .populate()
+    .secondPopulate("defaultProductVariantId");
+  const products = await features.query;
+  const count = await Product.countDocuments(req?.query?.filters);
+  return res.status(200).json({
+    success: true,
+    data: products,
+    count,
+  });
+});
+
+export const getOneProduct = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id).populate(
+    "defaultProductVariantId categoryId brandId"
+  );
+  if (!product) {
+    return next(new HandleERROR("Product not found", 404));
+  }
+  return res.status(200).json({
+    success: true,
+    data: product,
+  });
+});
+
+export const updateProduct = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const newProduct = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!newProduct) {
+    return next(new HandleERROR("Product not found", 404));
+  }
+  return res.status(200).json({
+    success: true,
+    data: newProduct,
+  });
+});
+
+export const deleteProduct = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const productVariants = await ProductVariant.deleteMany({ productId: id });
+  if (productVariants) {
+    return next(
+      new HandleERROR(
+        "Cannot delete productVariant with associated products",
+        400
+      )
+    );
+  }
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  if (!deletedProduct) {
+    return next(new HandleERROR("Product not found", 404));
+  }
+
+  for (let image of deletedProduct.imagesUrl) {
+    fs.unlinkSync(`${__dirname}/Public/${image}`);
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+});
