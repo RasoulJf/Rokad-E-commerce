9b6280ce6665fd6f59a5eb2922d675772c4dc164
RasoulJf <rasouljalilian8@gmail.com>
2025-04-05 11:34:22 +0330

    Update ProductMd.js

--- BackEnd/Models/ProductMd.js
+++ BackEnd/Models/ProductMd.js
@@ -1,7 +1,50 @@
 import mongoose from "mongoose";
-const productSchema=new mongoose.Schema({
- 
-    
-})
-const Product=mongoose.model("Product",productSchema)
-export default Product
+
+const informationSchema = new mongoose.Schema({
+  key: {
+    type: String,
+    required: [true, "key is required"],
+  },
+  value: {
+    type: String,
+    required: [true, "value is required"],
+  },
+});
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "title is required"],
+    trim: true,
+  },
+  imagesUrl: {
+    type: [String],
+    default: [],
+  },
+  information: {
+    type:[informationSchema],
+    default:[]
+  },
+  description:{
+    type: String,
+    required: [true, "desc is required"],
+    trim: true,
+  },
+  categoryId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Category',
+    required:[true,'Category is required']
+  },
+  brandId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Brand',
+    required:[true,'Brand is required']
+  },
+  defaultProductVariantId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'ProductVariant',
+    required:[true,'default variant is required']
+  }
+},{timestamps:true});
+const Product = mongoose.model("Product", productSchema);
+export default Product;
