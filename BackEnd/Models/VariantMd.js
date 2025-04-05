053c388dfa0e253184d766b5fc0b81ecde62f2c5
RasoulJf <rasouljalilian8@gmail.com>
2025-04-05 11:19:00 +0330

    Update VariantMd.js

--- BackEnd/Models/VariantMd.js
+++ BackEnd/Models/VariantMd.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-const variantSchema=new mongoose.Schema({
-    color:{
-        type:String
-    },
-    size:{
-        type:String
-    }
-})
-const Variant=mongoose.model("Variant",variantSchema)
-export default Variant
+const variantSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["size", "color"],
+    required: [true, "type is required"],
+  },
+  value: {
+    type: String,
+    required: [true, "value is required"],
+  },
+});
+const Variant = mongoose.model("Variant", variantSchema);
+export default Variant;
