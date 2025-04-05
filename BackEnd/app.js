69b235e4816a2480029ce063701c3b7d17f33dea
RasoulJf <rasouljalilian8@gmail.com>
2025-04-05 11:19:12 +0330

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -11,6 +11,8 @@ import addressRouter from "./Routes/Adddres.js";
 import brandsRouter from "./Routes/Brands.js";
 import categoryRouter from "./Routes/Category.js";
 import sliderRouter from "./Routes/Slider.js";
+import variantsRouter from "./Routes/Variant.js";
+import uploadRouter from "./Routes/upload.js";
 
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
@@ -20,12 +22,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("Public"));
+app.use('/api/upload',uploadRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/users',userRouter)
 app.use('/api/addresses',isLogin,addressRouter)
 app.use('/api/brands',brandsRouter)
 app.use('/api/categories',categoryRouter)
 app.use('/api/sliders',sliderRouter)
+app.use('/api/variants',variantsRouter)
 app.use("*", (req, res, next) => {
   next(new HandleERROR("Route not Found", 404));
 });
