ef44b8888dca0c5f5993c1b0553b42d3d6e2c695
RasoulJf <rasouljalilian8@gmail.com>
2025-04-05 11:34:12 +0330

    Update UserMd.js

--- BackEnd/Models/UserMd.js
+++ BackEnd/Models/UserMd.js
@@ -30,6 +30,6 @@ const userSchema=new mongoose.Schema({
         type:Boolean,
         default:false
     }
-})
+},{timestamps:true})
 const User=mongoose.model("User",userSchema)
 export default User
