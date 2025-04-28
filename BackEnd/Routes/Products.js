290d205cc9f8e8bbee491dffbe9c9cda3dd1c494
RasoulJf <rasouljalilian8@gmail.com>
2025-04-28 13:15:12 +0330

    Update Products.js

--- BackEnd/Routes/Products.js
+++ BackEnd/Routes/Products.js
@@ -1,14 +1,17 @@
 import express from "express";
 import {
   create,
+  favoriteProduct,
   getAll,
   getOne,
   remove,
   update,
 } from "../Controllers/ProductsCn.js";
 import { isAdmin } from "../Middlewares/isAdmin.js";
+import { isLogin } from "../Middlewares/isLogin.js";
 const productRouter = express.Router();
 productRouter.route("/").get(getAll).post(isAdmin, create);
+productRouter.route('/favorite').post(isLogin,favoriteProduct)
 productRouter
   .route("/:id")
   .get(getOne)
